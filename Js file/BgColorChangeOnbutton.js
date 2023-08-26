032946c6a47fedd34053fdc41874909d6c65ec2e
vishalarak <vishalarak1@gmail.com>
2023-08-26 16:50:56 +0530

    Change JS file structure

---
+++ Js file/BgColorChangeOnbutton.js
@@ -0,0 +1,21 @@
+function fun1(){
+    var btn = document.getElementById("btn").value;
+    document.bgColor = (btn);
+}
+function funred(){
+    var redbtn = document.getElementById("redbtn").value;
+    document.bgColor = (redbtn);
+}
+function funblue(){
+    var bluebtn = document.getElementById("bluebtn").value;
+    document.bgColor = (bluebtn);
+}
+function fungreen(){
+    var greenbtn = document.getElementById("greenbtn").value;
+    document.bgColor = (greenbtn);
+}
+
+function fun(){
+    var color = document.getElementById("coltxt").value;
+    document.bgColor = (color);
+}
